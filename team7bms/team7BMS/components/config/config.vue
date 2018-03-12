234e2f13f3a8046059ac0aa9906065c6f2d59c9b
FFZ <1501493678@qq.com>
2018-03-12 09:08:02 +0800

    更新login等等

---
+++ team7bms/team7BMS/components/config/config.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        
+        <datagrid :config="config" ></datagrid>
+    </div>
+</template>
+<script type="text/javascript">
+    import datagrid from '../datagrid/datagrid.vue'
+    export default {
+        components:{
+            datagrid
+        },
+        data(){
+            return {
+                config:{
+                    api:"http:10.3.136.21:7070/products",
+                    clos:['_id','url','brand','units','type','discount','hot','standards']
+                }
+            }
+        }
+    }
+</script>  
---
+++ team7bms/team7BMS/components/datagrid/datagrid.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <table>
+            <thead>
+                <th ></th>
+            </thead>
+        </table>
+    </div>
+</template>
+<script type="text/javascript">
+    export default{
+        
+    }
+
+</script>
